081df703bd60ca3e9ba668d85cf5cc857d94a407
Kofifus <mikagenic@gmail.com>
2017-07-17 13:11:36 +1000

    Update index.js

--- src/imageLoader/wadouri/index.js
+++ src/imageLoader/wadouri/index.js
@@ -3,7 +3,6 @@ import { getImagePixelModule,
          getModalityLUTOutputPixelRepresentation,
          getNumberValues,
          metaDataProvider } from './metaData/index.js';
-
 import dataSetCacheManager from './dataSetCacheManager.js';
 import fileManager from './fileManager.js';
 import getEncapsulatedImageFrame from './getEncapsulatedImageFrame.js';
