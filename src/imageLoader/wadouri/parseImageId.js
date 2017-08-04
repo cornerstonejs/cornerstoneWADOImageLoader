8061805922f9a1e401481a33ec26f248b40ab78b
DREYFUS ALOIS <alois.dreyfus@ido-in.com>
2017-08-04 13:09:56 +0200

    cornerstoneWADOImageLoader.parseImageId returns directly the imageId if it is already parsed

--- src/imageLoader/wadouri/parseImageId.js
+++ src/imageLoader/wadouri/parseImageId.js
@@ -1,6 +1,17 @@
 
-
+/**
+ * @param {Object|string} imageId
+ *    {Object}: { scheme, url, frame }
+ *    {string}: scheme:url[?frame={int}]
+ *
+ * @returns {Object} parsed imageId
+ */
 function parseImageId (imageId) {
+  // If the imageId is already parsed, return it directly
+  if (typeof imageId !== 'string') {
+    return imageId;
+  }
+
   // build a url by parsing out the url scheme and frame index from the imageId
   const firstColonIndex = imageId.indexOf(':');
   let url = imageId.substring(firstColonIndex + 1);
