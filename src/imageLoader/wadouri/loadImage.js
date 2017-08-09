f168d7196369f35deb44076c3c87c5fa56cda22a
DREYFUS ALOIS <alois.dreyfus@ido-in.com>
2017-08-09 11:23:04 +0200

    loadImageFromLoader() throws the error after rejecting the promise

--- src/imageLoader/wadouri/loadImage.js
+++ src/imageLoader/wadouri/loadImage.js
@@ -59,11 +59,13 @@ function loadImageFromPromise (dataSetPromise, imageId, frame, sharedCacheKey, o
         });
       });
     } catch (error) {
-      // Return the error, and the dataSet
+      // Return the error and the dataSet before throwing the error
       deferred.reject({
         error,
         dataSet
       });
+      // Throw the error
+      throw error;
     }
   }, function (error) {
     deferred.reject(error);
