b2a1dde5197c2d4e4af8f3e4ebb39894b70bf070
DREYFUS ALOIS <alois.dreyfus@ido-in.com>
2017-08-21 17:44:40 +0200

    _Add the decache function to the imagePromise instead of the image, because we can need to decache the dataSet even if the image is not constructed. _loadImageFromPromise() rejects the error and the dataSet if the creation of the image failed, instead of doing nothing or throwing an error

--- src/imageLoader/wadouri/loadImage.js
+++ src/imageLoader/wadouri/loadImage.js
@@ -9,10 +9,10 @@ import loadFileRequest from './loadFileRequest';
 import { xhrRequest } from '../internal';
 
 // add a decache callback function to clear out our dataSetCacheManager
-function addDecache (image) {
-  image.decache = function () {
+function addDecache (imagePromise, imageId) {
+  imagePromise.decache = function () {
     // console.log('decache');
-    const parsedImageId = parseImageId(image.imageId);
+    const parsedImageId = parseImageId(imageId);
 
     dataSetCacheManager.unload(parsedImageId.url);
   };
@@ -37,20 +37,35 @@ function loadImageFromPromise (dataSetPromise, imageId, frame, sharedCacheKey, o
   const deferred = $.Deferred();
 
   dataSetPromise.then(function (dataSet/* , xhr*/) {
-    const pixelData = getPixelData(dataSet, frame);
-    const transferSyntax = dataSet.string('x00020010');
-    const loadEnd = new Date().getTime();
-    const imagePromise = createImage(imageId, pixelData, transferSyntax, options);
+    try {
+      const pixelData = getPixelData(dataSet, frame);
+      const transferSyntax = dataSet.string('x00020010');
+      const loadEnd = new Date().getTime();
+      const imagePromise = createImage(imageId, pixelData, transferSyntax, options);
 
-    imagePromise.then(function (image) {
-      image.data = dataSet;
-      const end = new Date().getTime();
+      addDecache(imagePromise, imageId);
 
-      image.loadTimeInMS = loadEnd - start;
-      image.totalTimeInMS = end - start;
-      addDecache(image);
-      deferred.resolve(image);
-    });
+      imagePromise.then(function (image) {
+        image.data = dataSet;
+        const end = new Date().getTime();
+
+        image.loadTimeInMS = loadEnd - start;
+        image.totalTimeInMS = end - start;
+        deferred.resolve(image);
+      }, function (error) {
+        // Return the error, and the dataSet
+        deferred.reject({
+          error,
+          dataSet
+        });
+      });
+    } catch (error) {
+      // Return the error, and the dataSet
+      deferred.reject({
+        error,
+        dataSet
+      });
+    }
   }, function (error) {
     deferred.reject(error);
   });
