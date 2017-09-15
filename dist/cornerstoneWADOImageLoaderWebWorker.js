2403aa1319bfbfe86744bda7b62add63f507376d
Andre Botelho <andrebot_almeida@hotmail.com>
2017-09-15 10:25:58 -0300

    Moving lodash as a devDependency

--- dist/cornerstoneWADOImageLoaderWebWorker.js
+++ dist/cornerstoneWADOImageLoaderWebWorker.js
@@ -1,4 +1,4 @@
-/*! cornerstone-wado-image-loader - 0.14.7 - 2017-09-14 | (c) 2016 Chris Hafey | https://github.com/chafey/cornerstoneWADOImageLoader */
+/*! cornerstone-wado-image-loader - 0.14.7 - 2017-09-15 | (c) 2016 Chris Hafey | https://github.com/chafey/cornerstoneWADOImageLoader */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
