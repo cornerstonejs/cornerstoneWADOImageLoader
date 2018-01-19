024c34d884a9badcad785bdd2e84f8117743cc23
DREYFUS ALOIS <alois.dreyfus@ido-in.com>
2018-01-19 14:30:18 +0100

    cornerstoneWADOImageLoader.wadouri.loadImage(imageId, options): options has a default value = {}, instead of undefined

--- src/imageLoader/wadouri/loadImage.js
+++ src/imageLoader/wadouri/loadImage.js
@@ -108,7 +108,7 @@ function getLoaderForScheme (scheme) {
   }
 }
 
-function loadImage (imageId, options) {
+function loadImage (imageId, options = {}) {
   const parsedImageId = parseImageId(imageId);
   const loader = getLoaderForScheme(parsedImageId.scheme);
 
