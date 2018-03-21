76be466687d80b193f6fb5cdbb066ce23ae9e56d
DREYFUS ALOIS <alois.dreyfus@ido-in.com>
2018-03-21 11:14:39 +0100

    The cachecount attribute is saved next to the '''promise''' object, instead of in the '''promise''' object. => Fix the problem: '''promise.cacheCount''' triggered an exception if '''loadDICOMPromise''' was immediately resolved, because '"promise'" was not yet created.

--- src/imageLoader/wadouri/dataSetCacheManager.js
+++ src/imageLoader/wadouri/dataSetCacheManager.js
@@ -44,11 +44,15 @@ function load (uri, loadRequest = xhrRequest, imageId) {
     // console.log('returning existing load promise for ' + uri);
     promises[uri].cacheCount++;
 
-    return promises[uri];
+    return promises[uri].promise;
   }
 
   // This uri is not loaded or being loaded, load it via an xhrRequest
   const loadDICOMPromise = loadRequest(uri, imageId);
+
+  promises[uri] = {
+    cachecount: 1
+  };
 
   // handle success and failure of the XHR request load
   const promise = new Promise((resolve, reject) => {
@@ -66,7 +70,7 @@ function load (uri, loadRequest = xhrRequest, imageId) {
 
       loadedDataSets[uri] = {
         dataSet,
-        cacheCount: promise.cacheCount
+        cacheCount: promises[uri].cacheCount
       };
       cacheSizeInBytes += dataSet.byteArray.length;
       resolve(dataSet);
@@ -85,9 +89,9 @@ function load (uri, loadRequest = xhrRequest, imageId) {
     });
   });
 
-  promise.cacheCount = 1;
-
-  promises[uri] = promise;
+  if (promises[uri]) {
+    promises[uri].promise = promise;
+  }
 
   return promise;
 }
