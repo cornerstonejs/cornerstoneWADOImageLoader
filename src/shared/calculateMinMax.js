380f91600829459faefa1bcec28911e11b84fbe4
Zaid Safadi <zsafadi@hotmail.com>
2018-05-26 21:53:34 -0400

    Set strict mode for calculating the min/max value to true by default

--- src/shared/calculateMinMax.js
+++ src/shared/calculateMinMax.js
@@ -8,9 +8,10 @@ import getMinMax from './getMinMax.js';
  * Otherwise, correct them automatically.
  *
  * @param {Object} imageFrame
- * @param {Boolean} strict
+ * @param {Boolean} strict If 'strict' is true, log to the console a warning if these values do not match.
+ * Otherwise, correct them automatically.Default is true.
  */
-export default function calculateMinMax (imageFrame, strict = false) {
+export default function calculateMinMax (imageFrame, strict = true) {
   const minMax = getMinMax(imageFrame.pixelData);
   const mustAssign = !(isNumber(imageFrame.smallestPixelValue) && isNumber(imageFrame.largestPixelValue));
 
