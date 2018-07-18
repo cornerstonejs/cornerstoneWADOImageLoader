1e15a092449ec9bfbbe66da9f8125c4e2936f77d
maltempi <tmmaltempi@gmail.com>
2018-07-18 16:27:16 -0300

    Asseting pixeldata values on decoders_test

--- test/decoders_test.js
+++ test/decoders_test.js
@@ -1,66 +1,78 @@
 /* eslint import/extensions: 0 */
-import { expect } from 'chai';
-import { initializeJPEGLS } from '../src/shared/decoders/decodeJPEGLS.js';
-import { initializeJPEG2000 } from '../src/shared/decoders/decodeJPEG2000.js';
-import decodeImageFrame from '../src/imageLoader/decodeImageFrame.js';
-import getImageFrame from '../src/imageLoader/getImageFrame.js';
+import { should } from 'chai';
 import getPixelData from '../src/imageLoader/wadouri/getPixelData.js';
 import xhrRequest from '../src/imageLoader/internal/xhrRequest.js';
 import dataSetCacheManager from '../src/imageLoader/wadouri/dataSetCacheManager.js';
 import parseImageId from '../src/imageLoader/wadouri/parseImageId.js';
-import webWorkerManager from '../src/imageLoader/webWorkerManager.js';
+import createImage from '../src/imageLoader/createImage.js';
+import configure from '../src/imageLoader/configure.js';
+
+should();
 
 const transferSyntaxes = {
   '1.2.840.10008.1.2': 'LittleEndianImplicitTransferSyntax',
   '1.2.840.10008.1.2.1': 'LittleEndianExplicitTransferSyntax',
-  // TODO: dicomParser is failing with this.module in parseDicom
+  //  TODO: dicomParser is failing with this.module in parseDicom
   // '1.2.840.10008.1.2.1.99': 'DeflatedExplicitVRLittleEndianTransferSyntax',
 
-  '1.2.840.10008.1.2.2': 'BigEndianExplicitTransferSyntax', // Retired
+  '1.2.840.10008.1.2.2': 'BigEndianExplicitTransferSyntax',
 
-  // TODO: These three are failing
+  // Back and investigate! The compressing method (dcmtk) changes the pixels.
+  // is not an issue w/ our decoding library
   // '1.2.840.10008.1.2.4.50': 'JPEGProcess1TransferSyntax',
   // '1.2.840.10008.1.2.4.51': 'JPEGProcess2_4TransferSyntax',
+
+  // No decoder for transfer syntax 1.2.840.10008.1.2.4.53
   // '1.2.840.10008.1.2.4.53': 'JPEGProcess6_8TransferSyntax',
 
-  '1.2.840.10008.1.2.4.55': 'JPEGProcess10_12TransferSyntax',
+  // No decoder for transfer syntax 1.2.840.10008.1.2.4.55
+  // '1.2.840.10008.1.2.4.55': 'JPEGProcess10_12TransferSyntax',
+
   '1.2.840.10008.1.2.4.57': 'JPEGProcess14TransferSyntax',
   '1.2.840.10008.1.2.4.70': 'JPEGProcess14SV1TransferSyntax',
   '1.2.840.10008.1.2.4.80': 'JPEGLSLosslessTransferSyntax',
-  '1.2.840.10008.1.2.4.81': 'JPEGLSLossyTransferSyntax',
+
   '1.2.840.10008.1.2.4.90': 'JPEG2000LosslessOnlyTransferSyntax',
-  '1.2.840.10008.1.2.4.91': 'JPEG2000TransferSyntax',
   '1.2.840.10008.1.2.5': 'RLELosslessTransferSyntax'
 };
 
 const base = 'CTImage.dcm';
 const url = 'dicomweb://localhost:9876/base/testImages/';
 
-describe('decodeImageFrame', function () {
-  before(function () {
-    // Initialize the web worker manager
-    const config = {
-      maxWebWorkers: 1,
-      startWebWorkersOnDemand: true,
-      webWorkerPath: '/base/dist/cornerstoneWADOImageLoaderWebWorker.js',
-      taskConfiguration: {
-        decodeTask: {
-          loadCodecsOnStartup: true,
-          initializeCodecsOnStartup: false,
-          codecsPath: '/base/dist/cornerstoneWADOImageLoaderCodecs.js',
-          usePDFJS: false
-        }
+describe('Test lossless TransferSyntaxes decoding', function () {
+
+  let uncompressedPixelData = null;
+  let uncompressedImage = null;
+
+  before(function (done) {
+
+    // loads uncompressed study (the original one)
+    const imageId = `${url}${base}`;
+    const parsedImageId = parseImageId(imageId);
+
+    configure({
+      // callback allowing customization of the xhr (e.g. adding custom auth headers, cors, etc)
+      beforeSend (/* xhr, imageId */) { },
+      // callback allowing modification of newly created image objects
+      imageCreated (/* image */) { },
+      strict: false,
+      useWebWorkers: false,
+      decodeConfig: {
+        usePDFJS: false
       }
-    };
+    });
 
-    webWorkerManager.initialize(config);
+    dataSetCacheManager.load(parsedImageId.url, xhrRequest, imageId).then((dataSet) => {
+      const transferSyntax = dataSet.string('x00020010');
 
-    const decodeTask = {
-      usePDFJS: false
-    };
+      uncompressedPixelData = getPixelData(dataSet);
 
-    initializeJPEG2000(decodeTask);
-    initializeJPEGLS(decodeTask);
+      createImage(imageId, uncompressedPixelData, transferSyntax, {}).then((image) => {
+        uncompressedImage = image;
+      });
+
+      done();
+    }).catch(done);
   });
 
   after(function () {
@@ -72,22 +84,34 @@ describe('decodeImageFrame', function () {
     const filename = `${base}_${name}_${transferSyntaxUid}.dcm`;
 
     it(`should properly decode ${name}`, function (done) {
+      this.timeout(5000);
       const imageId = `${url}${filename}`;
       const parsedImageId = parseImageId(imageId);
       const dataSetPromise = dataSetCacheManager.load(parsedImageId.url, xhrRequest, imageId);
 
       dataSetPromise.then((dataSet) => {
         try {
-          const canvas = document.createElement('canvas');
-          const imageFrame = getImageFrame(imageId);
           const pixelData = getPixelData(dataSet);
-          const transferSyntax = dataSet.string('x00020010');
+          const curTransferSyntax = dataSet.string('x00020010');
 
-          decodeImageFrame(imageFrame, transferSyntax, pixelData, canvas);
+          curTransferSyntax.should.to.be.equals(transferSyntaxUid);
 
-          // TODO: Compare against known correct pixel data
-          expect(imageFrame).to.be.an('object');
-          done();
+          createImage(imageId, pixelData, curTransferSyntax, {}).then((image) => {
+            const uncompressedImagePixelData = uncompressedImage.getPixelData();
+            const curPixelData = image.getPixelData();
+
+            uncompressedImagePixelData.length.should.to.be.equals(curPixelData.length);
+
+            for (let i = 0; i < curPixelData.length - 1; i++) {
+              if (curPixelData[i] !== uncompressedImagePixelData[i]) {
+                curPixelData[i].should(`Pixel data is not equals in the position: ${i}`).
+                  to.equal(uncompressedImagePixelData[i]);
+              }
+            }
+
+            done();
+          }).catch(done);
+
         } catch (error) {
           done(error);
         }
