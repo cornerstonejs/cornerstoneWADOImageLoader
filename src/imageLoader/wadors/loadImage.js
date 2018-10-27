1b97363683d3960e6bb75222473239868252f3fb
Erik Ziegler <erik.sweed@gmail.com>
2018-10-27 08:30:21 +0200

    feat(content-type): Support WADO-RS in cases where transfer-syntax is not explicitly returned.

--- src/imageLoader/wadors/loadImage.js
+++ src/imageLoader/wadors/loadImage.js
@@ -8,27 +8,52 @@ import createImage from '../createImage.js';
  * @return The transfer-syntax as announced by the server, or Implicit Little Endian by default.
  */
 export function getTransferSyntaxForContentType (contentType) {
-  let transferSyntax = '1.2.840.10008.1.2'; // Default is Implicit Little Endian.
+  const defaultTransferSyntax = '1.2.840.10008.1.2'; // Default is Implicit Little Endian.
 
-  if (contentType) {
-    // Browse through the content type parameters
-    const parameters = contentType.split(';');
-
-    parameters.forEach((parameter) => {
-      // Look for a transfer-syntax=XXXX pair
-      const parameterValues = parameter.split('=');
-
-      if (parameterValues.length !== 2) {
-        return;
-      }
-
-      if (parameterValues[0].trim() === 'transfer-syntax') {
-        transferSyntax = parameterValues[1].trim() || transferSyntax;
-      }
-    });
+  if (!contentType) {
+    return defaultTransferSyntax;
   }
 
-  return transferSyntax;
+  // Browse through the content type parameters
+  const parameters = contentType.split(';');
+  const params = {};
+
+  parameters.forEach((parameter) => {
+    // Look for a transfer-syntax=XXXX pair
+    const parameterValues = parameter.split('=');
+
+    if (parameterValues.length !== 2) {
+      return;
+    }
+
+    params[parameterValues[0].trim()] = parameterValues[1].trim();
+  });
+
+  // This is useful if the PACS doesn't respond with a syntax
+  // in the content type.
+  // http://dicom.nema.org/medical/dicom/current/output/chtml/part18/chapter_6.html#table_6.1.1.8-3b
+  const defaultTransferSyntaxByType = {
+    'image/jpeg': '1.2.840.10008.1.2.4.70',
+    'image/x-dicom-rle': '1.2.840.10008.1.2.5',
+    'image/x-jls': '1.2.840.10008.1.2.4.80',
+    'image/jp2': '1.2.840.10008.1.2.4.90',
+    'image/jpx': '1.2.840.10008.1.2.4.92'
+  };
+
+  // dcm4che seems to be reporting the content type as just 'image/jp2'?
+  if (!Object.keys(params).length) {
+    return defaultTransferSyntaxByType[contentType];
+  }
+
+  if (params['transfer-syntax']) {
+    return params['transfer-syntax'];
+  }
+
+  if (params.type) {
+    return defaultTransferSyntaxByType[params.type];
+  }
+
+  return defaultTransferSyntax;
 }
 
 function loadImage (imageId, options) {
