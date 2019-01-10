faebde4e347dac63198f96ee68b2a00f08fcee70
Xuegui Yuan <yuanxuegui@huiyihuiying.com>
2019-01-10 11:14:37 +0800

    fix(pixelSpacing): use alternative tag  "x00181164" as the pixelSpacing

--- src/imageLoader/wadors/metaData/metaDataProvider.js
+++ src/imageLoader/wadors/metaData/metaDataProvider.js
@@ -34,7 +34,7 @@ function metaDataProvider (type, imageId) {
   if (type === 'imagePlaneModule') {
     const imageOrientationPatient = getNumberValues(metaData['00200037'], 6);
     const imagePositionPatient = getNumberValues(metaData['00200032'], 3);
-    const pixelSpacing = getNumberValues(metaData['00280030'], 2);
+    const pixelSpacing = getNumberValues(metaData['00280030'], 2) || getNumberValues(metaData['00181164'], 2);
     let columnPixelSpacing = null;
     let rowPixelSpacing = null;
 
