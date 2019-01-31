dcd33867aef40020ce6bba9e416215e722fc2cbd
Kofifus <mikagenic@gmail.com>
2019-01-31 16:23:52 +1100

    Dicom Overlays
    adds 'overlays' type returning an array of DICOM overlays
    
    http://dicom.nema.org/dicom/2013/output/chtml/part03/sect_C.9.html

--- src/imageLoader/wadouri/metaData/metaDataProvider.js
+++ src/imageLoader/wadouri/metaData/metaDataProvider.js
@@ -121,6 +121,44 @@ function metaDataProvider (type, imageId) {
       }
     };
   }
+
+  if (type === 'overlays') {
+    let overlays=[];
+
+    for (let overlayGroup = 0x00; overlayGroup <= 0x0f; overlayGroup += 0x01) {
+      let groupStr = 'x60'+overlayGroup.toString(16);
+      if (groupStr.length===4) groupStr = 'x600'+overlayGroup.toString(16);
+
+      const data = dataSet.elements[groupStr + '3000'];
+      if (!data) continue;
+      
+      let pixelData = [];
+      for (let i = 0; i < data.length; i++) {
+        for (var k = 0; k < 8; k++) {
+          const byte_as_int = dataSet.byteArray[data.dataOffset + i];
+          pixelData[i * 8 + k] = (byte_as_int >> k) & 0b1;
+        }
+      }
+
+      overlays.push({
+        rows: dataSet.uint16(groupStr + '0010'),
+        columns: dataSet.uint16(groupStr + '0011'),
+        type: dataSet.string(groupStr + '0040'),
+        x: dataSet.int16(groupStr + '0050', 1) - 1,
+        y: dataSet.int16(groupStr + '0050', 0) - 1,
+        pixelData,
+        description: dataSet.string(groupStr + '0022'),
+        label: dataSet.string(groupStr + '1500'),
+        roiArea: dataSet.string(groupStr + '1301'),
+        roiMean: dataSet.string(groupStr + '1302'),
+        roiStandardDeviation: dataSet.string(groupStr + '1303'),
+        fillStyle: 'white',
+        visible: true
+      });
+    }
+
+    return overlays;
+  }
 }
 
 export default metaDataProvider;
