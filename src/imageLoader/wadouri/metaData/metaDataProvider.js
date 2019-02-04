239e9c0998f89ca6b4193057abd9f15428f94535
Kofifus <mikagenic@gmail.com>
2019-02-04 15:30:07 +1100

    Update metaDataProvider.js

--- src/imageLoader/wadouri/metaData/metaDataProvider.js
+++ src/imageLoader/wadouri/metaData/metaDataProvider.js
@@ -125,7 +125,7 @@ function metaDataProvider (type, imageId) {
   if (type === 'overlayPlaneModule') {
     const overlays = [];
 
-    for (let overlayGroup = 0x00; overlayGroup <= 0x0f; overlayGroup += 0x01) {
+    for (let overlayGroup = 0x00; overlayGroup <= 0x1E; overlayGroup += 0x02) {
       let groupStr = `x60${overlayGroup.toString(16)}`;
       if (groupStr.length === 4) {
         groupStr = `x600${overlayGroup.toString(16)}`;
