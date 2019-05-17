5a1efb9cd0e1e25ccc689a2a93555657ea1b177e
Steven Aziz <steven_aziz@hotmail.com>
2019-05-17 13:35:20 -0700

    Parsing Dicom with an encapsulation boundary will no longer fail
    This fixes the issue with "loadDicomPromise" when the "dicomPart10AsArrayBuffer" contains an encapsulation boundary.
    If "dicomParser.parseDicom" fails, then the code simply tries to locate and remove the boundary, before calling the function again.

--- src/imageLoader/wadouri/dataSetCacheManager.js
+++ src/imageLoader/wadouri/dataSetCacheManager.js
@@ -1,5 +1,6 @@
 import external from '../../externalModules.js';
 import { xhrRequest } from '../internal/index.js';
+import findIndexOfString from '../wadors/findIndexOfString.js';
 
 /**
  * This object supports loading of DICOM P10 dataset from a uri and caching it so it can be accessed
@@ -11,6 +12,26 @@ let cacheSizeInBytes = 0;
 
 let loadedDataSets = {};
 let promises = {};
+
+function findBoundary (header) {
+  for (let i = 0; i < header.length; i++) {
+    if (header[i].substr(0, 2) === '--') {
+      return header[i];
+    }
+  }
+}
+
+function uint8ArrayToString (data, offset, length) {
+  offset = offset || 0;
+  length = length || data.length - offset;
+  let str = '';
+
+  for (let i = offset; i < offset + length; i++) {
+    str += String.fromCharCode(data[i]);
+  }
+
+  return str;
+}
 
 // returns true if the wadouri for the specified index has been loaded
 function isLoaded (uri) {
@@ -47,21 +68,82 @@ function load (uri, loadRequest = xhrRequest, imageId) {
     return promises[uri];
   }
 
+  function tryGetBoundarylessArrayBuffer(byteArray, dicomPart10AsArrayBuffer)
+  {
+    var tokenIndex = findIndexOfString(byteArray, '\r\n\r\n');
+
+    if (tokenIndex === -1) {
+      // no multipart mime header
+      return dicomPart10AsArrayBuffer;
+    }
+
+    var header = uint8ArrayToString(byteArray, 0, tokenIndex);
+    // Now find the boundary  marker
+
+    var split = header.split('\r\n');
+    var boundary = findBoundary(split);
+
+    if (!boundary) {
+      // no boundary marker
+      return dicomPart10AsArrayBuffer;
+    }
+
+    var offset = tokenIndex + 4; // skip over the \r\n\r\n
+    // find the terminal boundary marker
+    var endIndex = findIndexOfString(byteArray, boundary, offset);
+    if (endIndex === -1) {
+      // terminating boundary not found
+      return dicomPart10AsArrayBuffer;
+    }
+
+    // Remove \r\n from the length
+    var length = endIndex - offset - 2; // return the info for this pixel data
+    var boundarylessArrayBuffer = new Uint8Array(dicomPart10AsArrayBuffer, offset, length);
+    return boundarylessArrayBuffer;
+  }
+
   // This uri is not loaded or being loaded, load it via an xhrRequest
   const loadDICOMPromise = loadRequest(uri, imageId);
 
   // handle success and failure of the XHR request load
   const promise = new Promise((resolve, reject) => {
     loadDICOMPromise.then(function (dicomPart10AsArrayBuffer/* , xhr*/) {
-      const byteArray = new Uint8Array(dicomPart10AsArrayBuffer);
+      let byteArray = new Uint8Array(dicomPart10AsArrayBuffer);
 
       // Reject the promise if parsing the dicom file fails
       let dataSet;
 
+      let theError;
       try {
+        // if the byteArray contains an encapsulation boundary then this will fail!
         dataSet = dicomParser.parseDicom(byteArray);
       } catch (error) {
-        return reject(error);
+        theError = error;
+      }
+
+      if(dataSet == undefined) {
+        var didError = false;
+        try {
+            // try to remove the encapsulation boundary
+            byteArray = tryGetBoundarylessArrayBuffer(byteArray, dicomPart10AsArrayBuffer);
+        }
+        catch(error){
+            didError = true;
+        }
+
+        if(!didError) {
+          try {
+            // try parseDicom again, this time without the encapsulation boundary
+            dataSet = dicomParser.parseDicom(byteArray);
+          } 
+          catch (error) {
+            // no need
+          }
+        }
+      }
+
+      if(dataSet == undefined) {
+          return reject(theError);
       }
 
       loadedDataSets[uri] = {
