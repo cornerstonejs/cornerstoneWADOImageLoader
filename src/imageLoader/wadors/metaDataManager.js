6cb4e88ee4d7b3683fb98feee0ece8e1a162b139
rodrigobasilio2022 <rodrigo.basilio@radicalimaging.com>
2022-11-16 17:49:40 -0300

    Check if multiframe data exists

--- src/imageLoader/wadors/metaDataManager.js
+++ src/imageLoader/wadors/metaDataManager.js
@@ -3,12 +3,14 @@ import imageIdToURI from '../imageIdToURI.js';
 let metadataByImageURI = [];
 
 function getValue(tag, justElement = true) {
-  if (tag.Value) {
-    if (tag.Value[0] && justElement) {
-      return tag.Value[0];
-    }
+  if (tag) {
+    if (tag.Value) {
+      if (tag.Value[0] && justElement) {
+        return tag.Value[0];
+      }
 
-    return tag.Value;
+      return tag.Value;
+    }
   }
 
   return tag;
