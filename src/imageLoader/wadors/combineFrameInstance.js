5f77ab8bb7b972b9b39899ecdcb80804b9b3d5b5
rodrigobasilio2022 <rodrigo.basilio@radicalimaging.com>
2023-01-06 15:37:37 -0300

    Applying PR reviewer suggestions

--- src/imageLoader/wadors/combineFrameInstance.js
+++ src/imageLoader/wadors/combineFrameInstance.js
@@ -1,14 +1,4 @@
-function getTagValue(tag, justElement = true) {
-  if (tag && tag.Value) {
-    if (tag.Value[0] && justElement) {
-      return tag.Value[0];
-    }
-
-    return tag.Value;
-  }
-
-  return tag;
-}
+import getTagValue from './getTagValue.js';
 
 function getFrameInformation(
   PerFrameFunctionalGroupsSequence,
--- src/imageLoader/wadors/metaData/fixNMMetadata.js
+++ src/imageLoader/wadors/metaData/fixNMMetadata.js
@@ -1,14 +1,14 @@
+import getTagValue from '../getTagValue.js';
+
 export default function fixNMMetadata(metaData) {
   if (!metaData['00200032'] || metaData['00200037']) {
-    // adjust metadata in case of multiframe NM data, as the diucom tags
+    // adjust metadata in case of multiframe NM data, as the dicom tags
     // 00200032 and 00200037 could be found only in the dicom tag 00540022
-    if (
-      metaData['00540022'] &&
-      metaData['00540022'].Value &&
-      metaData['00540022'].Value.length > 0
-    ) {
-      metaData['00200032'] = metaData['00540022'].Value[0]['00200032'];
-      metaData['00200037'] = metaData['00540022'].Value[0]['00200037'];
+    const detectorInformationSequence = getTagValue(metaData['00540022']);
+
+    if (detectorInformationSequence) {
+      metaData['00200032'] = detectorInformationSequence['00200032'];
+      metaData['00200037'] = detectorInformationSequence['00200037'];
     }
   }
 
